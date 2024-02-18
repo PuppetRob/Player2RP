ac0e8da851aeab08ede2d4a76eb646165ef0f596
SeriousRob <rob@kcubye.com>
2024-02-17 18:12:57 -0600

    drugs + Businesses

---
+++ [extras]/[maps]/[businesses]/sbm-airshowroom/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Apollo developments'
+description 'Airport showroom'
+version '1.0.0'
+
+
+this_is_a_map 'yes'
---
+++ [extras]/[maps]/[drugs]/sbm-methlab/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "bodacious"
+game "gta5"
+
+lua54 "yes"
+this_is_a_map "yes"
+
+client_scripts {
+	"client.lua",
+	"cfg.lua"
+}
+
+escrow_ignore {
+  'cfg.lua',
+}
+
+dependency '/assetpacks'
