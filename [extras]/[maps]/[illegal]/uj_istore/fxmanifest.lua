1d4a7fea4519206877a7c755b36709fee656a86a
SeriousRob <rob@kcubye.com>
2024-02-17 18:10:32 -0600

    gangs + hotels + housing + illegal

---
+++ [extras]/[maps]/[illegal]/uj_istore/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version "bodacious"
+game "gta5"
+
+this_is_a_map "yes"
+
+data_file('DLC_ITYP_REQUEST')('stream/logos_store_qnx.ytyp')
+
+-- client_script 'client.lua'
+
+dependency '/assetpacks'
