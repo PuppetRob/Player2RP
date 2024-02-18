3a05e70b24d8d501d7a778b351e27b3bf40202dd
SeriousRob <rob@kcubye.com>
2024-02-17 19:53:17 -0600

    casino asseets

--- [extras]/[scripts]/[businesses]/jim-henhouse/config.lua
+++ [extras]/[scripts]/[businesses]/jim-henhouse/config.lua
@@ -1,4 +1,4 @@
-print("^2Jim^7-^2HenHouse ^7v^42^7.^40^7.^45 ^7- ^2HenHouse Script by ^1Jimathy^7")
+
 
 -- If you need support I now have a discord available, it helps me keep track of issues and give better support.
 
