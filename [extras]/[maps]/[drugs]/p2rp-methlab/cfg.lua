d62e5a60cfdc274d768b6b74e994021701cd5e32
SeriousRob <rob@kcubye.com>
2024-02-20 08:54:47 -0600

    methlab added back

---
+++ [extras]/[maps]/[drugs]/p2rp-methlab/cfg.lua
@@ -0,0 +1,27 @@
+cfg = {}
+
+cfg.ipl = {
+
+
+	["int_meth1"] = {
+		enable = true,
+		ymap = "int_meth1_milo_",
+		name = "int_meth1",
+		coords = vector3(-522.0983,-1743.3666,16.7261),
+		color = false,
+		colorSet = 1,
+		ipl = {
+			["meth_lab_setup"] = true,
+			["meth_lab_basic"] = false,
+			["meth_lab_upgrade"] = true,
+			["meth_lab_production"] = true,
+			["meth_lab_security_high"] = true,
+			["meth_lab_empty"] = false,
+		}
+	},
+
+
+
+}
+
+return cfg
