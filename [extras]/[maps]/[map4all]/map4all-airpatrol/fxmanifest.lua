25a348a4bcabf47baa782fd3f96236b1924d8f3d
SeriousRob <rob@kcubye.com>
2024-02-20 09:35:21 -0600

    map4allupdate

---
+++ [extras]/[maps]/[map4all]/map4all-airpatrol/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'bodacious'
+game 'gta5'
+this_is_a_map 'yes'
+
+author 'Map4All Interiors'
+description 'Los Santos Air Patrol'
+version '1.0.0'
+-- Release, last mod: 2/11/2024 1:02PM
+
+escrow_ignore {
+    'stream/basemap/ybn/*.ybn',
+    'stream/basemap/ydr/*.ydr',
+    'stream/basemap/ydr/*.ydd',
+    'stream/basemap/lodlight/*.ymap',
+    'stream/basemap/meta/*.ymap',  
+    'stream/custom/ytd/*.ytd'
+}
+
+dependency '/assetpacks'
