1c86c0f789c65945f7c9cc4476db7825696a173d
SeriousRob <rob@kcubye.com>
2024-02-21 19:37:35 -0600

    updated Map4all MLOS

---
+++ [extras]/[maps]/[map4all]/map4all-fightclub/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'bodacious'
+game 'gta5'
+this_is_a_map 'yes'
+
+author 'Map4All Interiors'
+description 'Fight Club Academy of Los Santos'
+version '1.0.1'
+
+escrow_ignore {
+    'stream/basemap/meta/*.ymap',
+    'stream/basemap/meta/*.ymf',
+    'stream/basemap/ybn/*.ybn',
+    'stream/basemap/ydr/*.ydr',
+    'stream/basemap/ydr/*.ydd',
+    'stream/basemap/ymf/*.ymt',
+    'stream/basemap/ynv/*.ynv',
+    
+    'stream/fightclub/ytd/*.ytd'
+}
+dependency '/assetpacks'
---
+++ [extras]/[maps]/[map4all]/map4all_scenarios/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+game "gta5"
+
+author 'Map4All Interiors'
+description 'Map4All Scenario Map Data'
+version '1.0.2'
+-- Modified los_puerta.ymt for Air Patrol
+
+file "sp_manifest.ymt"
+data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "sp_manifest.ymt"
+
+
+dependency '/assetpacks'
