ec22ff7c547b49c0ffdbf54fdcaa5eec263d8b22
SeriousRob <rob@kcubye.com>
2024-02-28 16:52:12 -0600

    eup

---
+++ [extras]/[eup]/sbm-business/fxmanifest.lua
@@ -0,0 +1,19 @@
+--- Generated with DurtyClothTool ---
+--- Created By TayMcKenzieNZ ---
+
+fx_version 'cerulean'
+game { 'gta5' }
+name "taysbusinesspack"
+description "Lore Friendly Clothing By TayMcKenzieNZ"
+author "TayMcKenzieNZ"
+version "1.4.0"
+
+files {
+  'mp_m_freemode_01_mp_m_taysbusinesspack.meta',
+  'mp_f_freemode_01_mp_f_taysbusinesspack.meta',
+  'stream/pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_taysbusinesspack.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_taysbusinesspack.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
