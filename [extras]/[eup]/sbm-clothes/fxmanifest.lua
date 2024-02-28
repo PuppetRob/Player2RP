d339e012ab05826e42122d87b908ba8c48c2a6e6
SeriousRob <rob@kcubye.com>
2024-02-28 16:56:52 -0600

    eup

---
+++ [extras]/[eup]/sbm-clothes/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'mp_f_freemode_01_p2rp.meta',
+	--'mp_f_freemode_01_p2rp_02.meta',
+	'mp_m_freemode_01_p2rp.meta',
+	--'mp_m_freemode_01_p2rp_02.meta',
+
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_p2rp.meta'
+--data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_p2rp_02.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_p2rp.meta'
+-- data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_p2rp_02.meta'
