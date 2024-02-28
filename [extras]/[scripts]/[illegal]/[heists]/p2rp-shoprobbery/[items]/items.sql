2014142250e0d76f0e4c01b2e10400a36afa268d
SeriousRob <rob@kcubye.com>
2024-02-28 15:46:33 -0600

    invis testing

--- [extras]/[scripts]/[illegal]/[heists]/p2rp-shoprobbery/[items]/items.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `items` (name, label, weight) VALUES
-  ('gold','Gold', 1),
-  ('diamond','Diamond', 1)
-;
--- [extras]/[scripts]/[smartphone]/qs-base/config/config.lua
+++
@@ -1,7 +0,0 @@
-Config = {}
-
-Config.IbanPrefix = 'QS'
-Config.WalletPrefix = 'QS'
-
-Config.NumberPrefix = '553' -- Prefix of the number
-Config.NumberDigits = 6     -- Amount of digits after the prefix
