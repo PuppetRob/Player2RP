3e54cb4c96145f98765ee159e1ab9e921d5d4631
SeriousRob <rob@kcubye.com>
2024-03-03 15:12:05 -0600

    dumpster diving rewards reduced

--- [extras]/[scripts]/[businesses]/jim-recycle/server/dumpsterserver.lua
+++ [extras]/[scripts]/[businesses]/jim-recycle/server/dumpsterserver.lua
@@ -8,16 +8,16 @@ end)
 RegisterServerEvent('jim-recycle:Dumpsters:Reward', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-	for i = 1, math.random(1, 4) do
+	for i = 1, math.random(1, 1) do
 		local item = Config.DumpItems[math.random(1, #Config.DumpItems)]
-		local amount = math.random(1, 3)
+		local amount = math.random(1, 1)
 		Player.Functions.AddItem(item, amount)
 		TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[item], 'add', amount)
 		Citizen.Wait(100)
 	end
 	--If two random numbers match, give reward
-	if math.random(1, 4) == math.random(1, 4) then
-		local random = math.random(1, 4)
+	if math.random(1, 4) == math.random(1, 1) then
+		local random = math.random(1, 1)
 		Player.Functions.AddItem("rubber", random)
 		TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["rubber"], 'add', random)
 	end
