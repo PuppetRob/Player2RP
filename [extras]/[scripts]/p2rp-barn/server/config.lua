e12a92493dfd8cb0a1474262fb256516a62cce80
SeriousRob <rob@kcubye.com>
2024-03-16 13:29:47 -0500

    barn find added

---
+++ [extras]/[scripts]/p2rp-barn/server/config.lua
@@ -0,0 +1,32 @@
+Config = {
+    VehicleName = 'sultanrs',
+    VehicleLoc = {
+        x = -31.03,
+        y = 3028.25,
+        z = 40.35,
+        h = 133.25,
+    }, 
+
+    
+
+    VehicleCost = {
+        copper = 100,
+        aluminum = 100,
+        steel = 100,
+        plastic = 100,
+        glass = 100,
+        rubber = 100,
+    }, 
+    ShopLoc = {
+        x = 103.74023,
+        y = 6622.703,
+        z = 31.2017,
+        h = 224.12,
+    },
+    IsClaimed = false,
+    Winner = nil,
+    WinnerCid = nil,
+    Bop = false,
+}
+
+
