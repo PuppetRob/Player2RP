abd33fd9975206105fe4d022dcd65eb438f7466c
SeriousRob <rob@kcubye.com>
2024-03-19 06:33:08 -0500

    vodka added back to pizza this

--- [extras]/[scripts]/[businesses]/jim-pizzathis/config.lua
+++ [extras]/[scripts]/[businesses]/jim-pizzathis/config.lua
@@ -53,6 +53,7 @@ Config = {
 			{ name = "water_bottle", price = 0, amount = 50, info = {}, type = "item", slot = 1, },
 			{ name = "coffee", price = 0, amount = 50, info = {}, type = "item", slot = 2, },
 			{ name = "kurkakola", price = 0, amount = 50, info = {}, type = "item", slot = 3, },
+			{ name = "vodka", price = 0, amount = 50, info = {}, type = "item", slot = 4, },
 
 		}
 	},
